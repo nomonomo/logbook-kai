e031996c8088035a76f4b42b04c7bc7ecc3ac486
nomonomo <snomoto@af.airnet.ne.jp>
2022-07-10 16:59:23 +0900

    2022春梅雨イベント札対応

--- src/main/java/logbook/internal/SeaArea.java
+++ src/main/java/logbook/internal/SeaArea.java
@@ -8,16 +8,16 @@ import java.util.stream.Stream;
  */
 public enum SeaArea {
 
-    識別札1("Ｒ方面防備部隊", 1),
-    識別札2("進出第一陣", 2),
-    識別札3("進出第二陣", 3),
-    識別札4("機動部隊", 4),
-    識別札5("方面護衛隊", 5),
-    識別札6("識別札6", 6),
-    識別札7("識別札7", 7),
-    識別札8("識別札8", 8),
-    識別札9("識別札9", 9),
-    識別札10("識別札10", 10);
+    識別札1("Ｒ方面防備部隊", 1, 4),
+    識別札2("進出第一陣", 2, 8),
+    識別札3("進出第二陣", 3, 10),
+    識別札4("機動部隊", 4, 12),
+    識別札5("方面護衛隊", 5, 14),
+    識別札6("呉防備戦隊", 6, 16),
+    識別札7("第二艦隊", 7, 18),
+    識別札8("機動部隊別動隊", 8, 20),
+    識別札9("佐世保配備艦隊", 9, 22),
+    識別札10("連合救援艦隊", 10, 5);
 
     /** 名前 */
     private String name;
@@ -25,9 +25,16 @@ public enum SeaArea {
     /** 海域(イベント海域のお札) */
     private int area;
 
-    SeaArea(String name, int area) {
+
+    /** お札アイコンのNo
+     *  resources\common\common_event
+     *  */
+    private int imageNo;
+    
+    SeaArea(String name, int area, int imageNo) {
         this.name = name;
         this.area = area;
+        this.imageNo = imageNo;
     }
 
     /**
@@ -44,6 +51,14 @@ public enum SeaArea {
      */
     public int getArea() {
         return this.area;
+    }
+
+    /**
+     * お札アイコンのNoを取得します。
+     * @return お札アイコンのNo
+     */
+    public int getImageId() {
+        return this.imageNo;
     }
 
     @Override
