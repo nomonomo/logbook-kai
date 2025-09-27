952480d77f51ad997fea671ec1dda224283eaa3e
nomonomo <snomoto@af.airnet.ne.jp>
2025-09-27 17:16:40 +0900

    UTF-8対応済みの為、呼び出していないロジック削除

--- logbook/src/main/java/logbook/Messages.java
+++ logbook/src/main/java/logbook/Messages.java
@@ -1,18 +1,7 @@
 package logbook;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.security.AccessController;
-import java.security.PrivilegedActionException;
-import java.security.PrivilegedExceptionAction;
 import java.text.MessageFormat;
 import java.util.Locale;
-import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 import logbook.internal.LoggerHolder;
@@ -20,7 +9,7 @@ import logbook.plugin.PluginServices;
 
 /**
  * 国際化対応
- *
+ * UTF-8でエンコードされている。
  */
 public class Messages {
 
@@ -71,54 +60,5 @@ public class Messages {
      */
     public static ResourceBundle getResourceBundle() {
         return RESOURCE_BUNDLE;
-    }
-
-    /**
-     * UTF-8でpropertiesファイルを読むためのResourceBundle.Control
-     */
-    private static final class UTF8Control extends ResourceBundle.Control {
-
-        @Override
-        public ResourceBundle newBundle(
-                String baseName,
-                Locale locale,
-                String format,
-                ClassLoader loader,
-                boolean reload)
-                throws IOException {
-
-            String bundleName = this.toBundleName(baseName, locale);
-            String resourceName = this.toResourceName(bundleName, "properties"); //$NON-NLS-1$
-
-            // get InputStream
-            InputStream is;
-            try {
-                is = AccessController.doPrivileged((PrivilegedExceptionAction<InputStream>) () -> {
-                    InputStream stream = null;
-                    if (reload) {
-                        URL url = loader.getResource(resourceName);
-                        if (url != null) {
-                            URLConnection con = url.openConnection();
-                            con.setUseCaches(false);
-                            stream = con.getInputStream();
-                        }
-                    } else {
-                        stream = loader.getResourceAsStream(resourceName);
-                    }
-                    return stream;
-                });
-            } catch (PrivilegedActionException e) {
-                throw (IOException) e.getException();
-            }
-            if (is != null) {
-                try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
-                    try (BufferedReader reader = new BufferedReader(isr)) {
-                        return new PropertyResourceBundle(reader);
-                    }
-                }
-            } else {
-                return null;
-            }
-        }
     }
 }
